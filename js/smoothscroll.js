c25a538470ee00e49e5bb3351efeebf90637bd15
Daniel5747 <stephensondaniel93@gmail.com>
2015-10-27 01:50:36 -0400

    new format

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -57,3 +57,23 @@ $("#back-to-top a[href^='#']").on('click', function(e) {
      });
 
 });
+
+$("#bottom a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top -50
+     }, 1000, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
